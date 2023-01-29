f74d798358f6940bdb9979a25e4e202bc9aff368
blevinspercussion <blevinspercussion@gmail.com>
2023-01-29 13:26:27 -0500

    Basic styling in place for nav menus

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get DOM Elements
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const mimiHamburger = document.getElementById('mimi-menu-button');
+const rickHamburger = document.getElementById('rick-menu-button');
+const mimiNav = document.getElementById('mimi-nav');
+const rickNav = document.getElementById('rick-nav');
+
