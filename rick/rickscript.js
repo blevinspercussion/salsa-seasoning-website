91f54b9c961c1bf3c2e263e7f4657f6e1c632de4
blevinspercussion <blevinspercussion@gmail.com>
2023-02-01 14:08:48 -0500

    Moved header and nav elements into a custem web component 'desktop-headen'

---
+++ rick/rickscript.js
@@ -0,0 +1,9 @@
+// Get DOM elements
+
+const body = document.querySelector('body');
+const aboutSection = document.getElementById('about-section');
+const aboutButton = document.getElementById('about-button');
+const salsasButton = document.getElementById('salsas-button');
+const seedsButton = document.getElementById('seeds-button');
+const recipesButton = document.getElementById('recipes-button');
+
--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+/////////////////////////////////
+// Define custom web compenents /
+/////////////////////////////////
+
+class DesktopHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+    <img class="hamburger" id="mimi-menu-button" src="./media/pics/hamburger.png" alt="mimi's kitchen menu button">
+    <a href="#"><img class="logo" src="./media/pics/mimis_kitchen_logo.png" alt="mimi's kitchen logo"></a>
+    <div class="vertical-line"></div>
+    <a href="./rick/rickhome.html"><img class="logo rick-logo" src="./media/pics/ricks_salsa_logo.png" alt="rick's salsa logo"></a>
+    <img class="hamburger" id="rick-menu-button" src="./media/pics/hamburger.png" alt="rick's salsa menu button">
+<nav id="mimi-nav" class="mimi-nav-hidden">
+    <div>
+        <img class="logo" src="./media/pics/mimis_kitchen_logo.png" alt="mimi's kitchen logo">
+    </div>
+    <ul id="mimi-menu">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Seasonings</a></li>
+        <li><a href="#">Spice Blends</a></li>
+        <li><a href="#">Recipes</a></li>
+    </ul>
+</nav>
+<nav id="rick-nav" class="rick-nav-hidden">
+    <div>
+        <img class="logo rick-logo" src="./media/pics/ricks_salsa_logo.png" alt="rick's salsa logo">
+    </div>
+    <ul id="rick-menu">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Salsas</a></li>
+        <li><a href="#">Seeds</a></li>
+        <li><a href="#">Recipes</a></li>
+    </ul>
+</nav>
+        `
+    }
+}
+
+customElements.define('desktop-header', DesktopHeader);
+
+
 // Get DOM Elements
 const body = document.querySelector('body');
 const header = document.querySelector('header');
@@ -10,6 +54,8 @@ const overlay = document.getElementById('overlay');
 const contactPopup = document.getElementById('contact-popup');
 const popupExitButton = document.getElementById('popup-exit-button');
 const submitButton = document.getElementById('submit');
+
+
 
 mimiHamburger.addEventListener('click', () => {
     if (rickNav.classList.contains('rick-nav-shown')) {
@@ -77,15 +123,16 @@ submitButton.addEventListener('click', (e) => {
 
 // Intersection observer for home page 
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     });
+// });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
+
