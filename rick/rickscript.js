d065be54b4032d76a04085e0e38bc32c8f200a4a
blevinspercussion <blevinspercussion@gmail.com>
2023-02-02 20:59:17 -0500

    Added css styling and js functionality to nav menu of rickhome.html

--- rick/rickscript.js
+++ rick/rickscript.js
@@ -1,9 +1,16 @@
 // Get DOM elements
 
 const body = document.querySelector('body');
-const aboutSection = document.getElementById('about-section');
-const aboutButton = document.getElementById('about-button');
-const salsasButton = document.getElementById('salsas-button');
-const seedsButton = document.getElementById('seeds-button');
-const recipesButton = document.getElementById('recipes-button');
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+    if (navMenu.classList.contains('hidden-element')) {
+        navMenu.classList.remove('hidden-element');
+        hamburger.src = "../media/pics/exit.svg"
+    } else {
+        navMenu.classList.add('hidden-element');
+        hamburger.src = "../media/pics/hamburger.png";
+    }
+})
 
